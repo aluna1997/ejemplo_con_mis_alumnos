3527383d7e565c65919c95a4d7d00c90abf44253
“Felipe <aluna1997@ciencias.unam.mx>
2023-11-23 21:00:05 -0600

    Mi primer commit.

---
+++ ejercicio_clase.js
@@ -0,0 +1,17 @@
+// Requerimientos: Solicita las coordenadas (x, y) de un punto e 
+// indica en qué cuadrante se encuentra.
+
+var x = 0;
+var y = 1;
+
+if (x > 0 && y > 0) {
+    console.log("El punto se encuentra en el primer cuadrante.");
+} else if (x < 0 && y > 0) {
+    console.log("El punto se encuentra en el segundo cuadrante.");
+} else if (x < 0 && y < 0) {
+    console.log("El punto se encuentra en el tercer cuadrante.");
+} else if (x > 0 && y < 0) {
+    console.log("El punto se encuentra en el cuarto cuadrante.");
+} else {
+    console.log("El punto esta sobre un eje.");
+}
